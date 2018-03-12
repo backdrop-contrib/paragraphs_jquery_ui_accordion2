b2dd87e936283ecf064e0e434b7b88934eb96e6f
Maksym Shakhrai <maxim.trickster@gmail.com>
2018-03-12 12:05:45 +0200

    fixed support of several accordions on the same page; refactored a bit js code

--- js/paragraphs_jquery_ui_accordion.js
+++ js/paragraphs_jquery_ui_accordion.js
@@ -1,51 +1,66 @@
 /**
  * @file
- * Paragraphs accordion script
+ * Paragraphs accordion script.
  */
 
 (function ($) {
   Drupal.behaviors.paragraphs_jquery_ui_accordion = {
     attach: function (context, settings) {
-      var accordion_id = '#' + Drupal.settings.paragraphs_jquery_ui_accordion.id;
+      var ids = Drupal.settings.paragraphs_jquery_ui_accordion.ids;
       var autoscroll = Drupal.settings.paragraphs_jquery_ui_accordion.autoscroll;
 
+      // Determine initial state of accordion paragraphs (from hash or first item).
       if (window.location.hash) {
         var activeParagraph = false;
       } else {
         var activeParagraph = 0;
       }
 
-      $(accordion_id, context).accordion({
-        active: activeParagraph,
-        collapsible: true,
-        animated: 'slide',
-        autoHeight: false,
-        navigation: true,
-        heightStyle: "content"
+      // Initialization of each accordion.
+      $.each(ids, function( index, accordion_id ) {
+        accordion_id = '#' + accordion_id;
+        $(accordion_id, context).accordion({
+          active: activeParagraph,
+          collapsible: true,
+          animated: 'slide',
+          autoHeight: false,
+          navigation: true,
+          heightStyle: "content"
+        });
+
+        if (autoscroll === 1) {
+          $(accordion_id, context).on( "accordionactivate", function( event, ui ) {
+            var newHash = $(ui.newHeader).find('a').attr('href');
+            autoScroll(newHash);
+          });
+        }
+
+        // Open content that matches the hash.
+        $( window ).on('hashchange', function() {
+          activateParagraph(accordion_id);
+        }).trigger('hashchange');
       });
 
-      $(accordion_id, context).on( "accordionactivate", function( event, ui ) {
-        var newHash = $(ui.newHeader).find('a').attr('href');
-        changeHash(newHash);
-      });
-
-      // open content that matches the hash
-      $( window ).on('hashchange', function() {
-        activateParagraph(accordion_id);
-      }).trigger('hashchange');
-
-      function changeHash(newHash) {
+      /**
+       * AutoScroll function.
+       *
+       * @param newHash
+       */
+      function autoScroll(newHash) {
         if (newHash !== 'undefined' && newHash) {
           var target = $(newHash);
-          if (autoscroll === 1) {
-            $('html, body').animate({
-              scrollTop: target.offset().top - 50
-            }, 250);
-          }
+          $('html, body').animate({
+            scrollTop: target.offset().top - 50
+          }, 250);
           return false;
         }
       }
 
+      /**
+       * Helper function for activation accordion paragraphs from hash.
+       *
+       * @param accordion_id
+       */
       function activateParagraph(accordion_id) {
         var hash = window.location.hash;
         if (hash) {
